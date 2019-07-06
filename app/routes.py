3c88926c6a9ed0951a09b6f36815e210c3ebe8b3
raldenprog <raldenprog@mail.ru>
2019-07-07 02:34:41 +0700

    на роуте /map отображение нескольких маршрутов с подкраской.

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,7 @@
 from app import app
 from flask import render_template
 from config import GOOGLEMAPS_KEY
+import json
 
 
 @app.route('/', methods=['GET'])
@@ -16,5 +17,29 @@ def index():
                            waypoints_y=waypoints_y,
                            lat=55.020790,
                            lng=82.923879,
+                           GOOGLEMAPS_KEY=GOOGLEMAPS_KEY
+                           )
+
+
+@app.route('/map', methods=['GET'])
+def map():
+    start_x = [55.008588, 55.004583, 55.003973, 55.023319, 55.011743]
+    start_y = [82.9422249, 82.945431, 82.943901, 82.945089, 82.935623]
+    end_x = [55.0184809, 55.004621, 55.019223, 55.011126, 54.998835]
+    end_y = [82.9269283, 82.973148, 82.938468, 82.976636, 82.956684]
+    waypoints_x = [[55.019960], [55.005725], [55.013949], [55.018689], [55.001317]]
+    waypoints_y = [[82.936605], [82.970770], [82.950834], [82.972365], [82.950325]]
+    rating = [5, 1, 2, 3, 4]
+
+    return render_template("map.html",
+                           start_x=start_x,
+                           start_y=start_y,
+                           end_x=end_x,
+                           end_y=end_y,
+                           waypoints_x=waypoints_x,
+                           waypoints_y=waypoints_y,
+                           lat=55.020790,
+                           lng=82.923879,
+                           rating=rating,
                            GOOGLEMAPS_KEY=GOOGLEMAPS_KEY
                            )
