a385c418589ac37f757dac791a9f16430e6b3198
raldenprog <raldenprog@mail.ru>
2019-07-07 14:22:07 +0700

    карта связанна с бэк ендом

--- app/routes.py
+++ app/routes.py
@@ -1,62 +1,49 @@
+import requests
+import json
 from app import app
-from flask import jsonify
-from app.api.route import Route
-
 from flask import render_template
-from config import GOOGLEMAPS_KEY
-
-
-@app.route('/driver', methods=['GET'])
-def driver():
-    start = '55.008588,82.9422249'
-    end = '55.0184809,82.9269283'
-    waypoints_x = [55.019960]
-    waypoints_y = [82.936605]
-    return render_template("driver.html",
-                           start=start,
-                           end=end,
-                           waypoints_x=waypoints_x,
-                           waypoints_y=waypoints_y,
-                           lat=55.020790,
-                           lng=82.923879,
-                           GOOGLEMAPS_KEY=GOOGLEMAPS_KEY
-                           )
-
-
-@app.route('/')
-@app.route('/index')
-def index():
-    return "Hello, World!"
+from config import GOOGLEMAPS_KEY, HOST
 
 
 @app.route('/route/all')
 def route_all():
-    return jsonify(Route().get_all_route_bus())
+    r = requests.get('{}/route/all'.format(HOST))
+    roads = json.loads(r.text)
+    start_x = []
+    start_y = []
+    end_x = []
+    end_y = []
+    rating = []
 
-
-@app.route('/route/<int:id_route>')
-def route_one(id_route):
-    return jsonify(Route().get_route_list(id_route))
-
-@app.route('/map', methods=['GET'])
-def map():
-    start_x = [55.008588, 55.004583, 55.003973, 55.023319, 55.011743]
-    start_y = [82.9422249, 82.945431, 82.943901, 82.945089, 82.935623]
-    end_x = [55.0184809, 55.004621, 55.019223, 55.011126, 54.998835]
-    end_y = [82.9269283, 82.973148, 82.938468, 82.976636, 82.956684]
-    waypoints_x = [[55.019960], [55.005725], [55.013949], [55.018689], [55.001317]]
-    waypoints_y = [[82.936605], [82.970770], [82.950834], [82.972365], [82.950325]]
-    rating = [5, 1, 2, 3, 4]
+    for r in range(10):
+        start_x.append(roads[r].get('start_x'))
+        start_y.append(roads[r].get('start_y'))
+        end_x.append(roads[r].get('end_x'))
+        end_y.append(roads[r].get('end_y'))
+        rating.append(roads[r].get('Индикация'))
 
     return render_template("map.html",
                            start_x=start_x,
                            start_y=start_y,
                            end_x=end_x,
                            end_y=end_y,
-                           waypoints_x=waypoints_x,
-                           waypoints_y=waypoints_y,
                            lat=55.020790,
                            lng=82.923879,
                            rating=rating,
+                           GOOGLEMAPS_KEY=GOOGLEMAPS_KEY
+                           )
+
+
+@app.route('/route/<int:id_route>')
+def route_one(id_route):
+    r = requests.get('{}/route/{}'.format(HOST, id_route))
+    road = json.loads(r.text)
+    return render_template("driver.html",
+                           start=road.get('start'),
+                           end=road.get('end'),
+                           waypoints_x=road.get('waypoints_x'),
+                           waypoints_y=road.get('waypoints_y'),
+                           lat=55.020790,
+                           lng=82.923879,
                            GOOGLEMAPS_KEY=GOOGLEMAPS_KEY
                            )
--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 CSRF_ENABLED = True
 SECRET_KEY = 'hard_password'
 GOOGLEMAPS_KEY = 'AIzaSyDEsraxsTwvKfGueWg3sL-XOjgMWIR2Ltc'
+HOST = 'http://10a7b29c.ngrok.io'
 database = {
     "dbname": "postgres",
     "user": "postgres",
